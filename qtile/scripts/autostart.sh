c3f3bbaa0d72d44bc79cc28f96dfa00a12b2493b
0X45R <robertplawski8@gmail.com>
2022-11-24 16:16:55 +0100

    Updated qtile

--- qtile/scripts/autostart.sh
+++ qtile/scripts/autostart.sh
@@ -2,5 +2,5 @@ picom &
 wal -R 
 setxkbmap -model pc104 -layout us,pl -variant ,, -option grp:alt_shift_toggle 
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-xrandr --output DisplayPort-0 --noprimary --auto --left-of HDMI-A-0
-xrandr --output HDMI-A-0 --mode "1920x1080" --primary
+xrandr --output HDMI-A-0 --noprimary --auto --left-of DisplayPort-0
+xrandr --output DisplayPort-0 --mode "1920x1080" --primary
